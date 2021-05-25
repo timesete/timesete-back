8ce5c160b5290ee6640f59b5e5e4e891c900fb8a
Jeferson Gonçalves de Freitas <freitas000jeferson@gmail.com>
2021-05-25 18:17:48 -0300

    ajuste pra conseguir exportar banco

--- src/config/database/sequelize.js
+++ src/config/database/sequelize.js
@@ -2,15 +2,20 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({ path: path.join(__dirname, '../../../.env') });
+const { env } = require('../../../env');
 
 module.exports = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database:
-    process.env.NODE_ENV !== 'test'
-      ? process.env.DB_NAME
-      : `${process.env.DB_NAME}-test`,
-  host: process.env.DB_HOST,
+  username: env.DB_USER,
+  password: env.DB_PASS,
+  database: env.DB_NAME,
+  host: env.DB_HOST,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database:
+  //   process.env.NODE_ENV !== 'test'
+  //     ? process.env.DB_NAME
+  //     : `${process.env.DB_NAME}-test`,
+  // host: process.env.DB_HOST,
   dialect: 'postgres',
   // protocol: 'postgres',
   // ssl: true,
